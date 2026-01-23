a2f6b16c625c3f458ef73eade5f7807d8e3e9396
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-01-23 00:16:57 -0800

    Update models.py

--- recommender/models.py
+++ recommender/models.py
@@ -73,6 +73,9 @@ class UserRating(models.Model):
     class Meta:
         unique_together = ['user', 'song']
 
+    def __str__(self):
+        return f"{self.user.username} - {self.song.title}: {self.rating}"
+
 class PlayHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
